7f139677eef2d3b686df1d8ffe253e4a4a26dea6
mravaux <mravaux@gmail.com>
2011-05-13 10:34:14 +0200

    added admin account edition, users management boiler(NOT WORKING YETrake routes) and beginnig to scaffold some groups options to sort user (NOT WORKING TOO)

---
+++ app/controllers/admin/groups_controller.rb
@@ -0,0 +1,37 @@
+module Admin
+  class GroupsController < BaseController
+
+    defaults :instance_name => 'site'
+
+    sections 'groups', 'site'
+
+    actions :edit, :update
+
+    respond_to :json, :only => :update
+    
+    def index
+      @groups = Group.all
+    end
+    
+    def update
+      update! do |success, failure|
+        success.html { redirect_to edit_admin_current_site_url(new_host_if_subdomain_changed) }
+      end
+    end
+
+    protected
+
+    def resource
+      @site = current_site
+    end
+
+    def new_host_if_subdomain_changed
+      if !Locomotive.config.manage_subdomain? || @site.domains.include?(request.host)
+        {}
+      else
+        { :host => site_url(@site, { :fullpath => false, :protocol => false }) }
+      end
+    end
+
+  end
+end
